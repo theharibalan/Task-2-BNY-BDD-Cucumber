5c1afddffb25d36b3bfa350c1d162ff646d31314
theharibalan <srs.haribalan2003@gmail.com>
2025-03-24 10:08:50 +0530

    featurefiles exec order solved

--- src/test/java/com/hbn/apitesting/Runners/ProductRunner.java
+++ src/test/java/com/hbn/apitesting/Runners/ProductRunner.java
@@ -3,22 +3,34 @@ package com.hbn.apitesting.Runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.AfterClass;
+import org.junit.FixMethodOrder;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/com/hbn/apitesting/Features",
+        features = {
+                // feature file order specified -> This alone is not working
+                "src/test/java/com/hbn/apitesting/Features/ProductPostGet.feature",
+                "src/test/java/com/hbn/apitesting/Features/ProductUpdate.feature",
+                "src/test/java/com/hbn/apitesting/Features/ProductDelete.feature" },
+
         glue = "com.hbn.apitesting.StepDefinitions", // Ensure correct package name
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/report.html",
                 "json:target/cucumber-reports/report.json"
         },
-        monochrome = true
+        monochrome = true,
+        dryRun = true
 )
+
+//forcing to run in the specified order
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProductRunner {
 
     @AfterClass
